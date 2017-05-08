ad67ffcbc7f8afbb0301d30ae33346d76a28b066
Beren İlkim Ceylan <berenilkim.ceylan@v-count.com>
2017-05-08 22:32:09 +0300

    Dynamic sinlge, array or csv mail add function

--- js/MultiMailPlugin.js
+++ js/MultiMailPlugin.js
@@ -1,31 +1,49 @@
 (function ($) {
     var result = [];
+    var input;
     $.fn.multiMailInput = function () {
+        var $this = this;
+        $this.addMail = function (mail, elem) {
+            var condition = validateMail(mail, result)["condition"];
+            var msg = validateMail(mail, result)["msg"];
+            if(condition){
+                elem.siblings(".mmBoxes").append("<span class='mmBox' style='height: 17px; vertical-align: center; width: 300px;'>" + mail + " <img class='deleteBox' width=16 src='image/delete.png' style='cursor: pointer; float: right;'/></span>");
+                $.fn.multiMailInput.arrangeBoxCss(".mmBox");
+                result.push(mail);
+                input.val("");
+                $(".deleteBox").click(function () {
+                    deleteMMBox(this);
+                });
+            }else{
+               alert(msg)
+            }
+            
+        },
+        $this.addMailsArray = function (mailArray, elem) {
+            for (var i in mailArray) {
+                $this.addMail(mailArray[i],elem);
+            }
+        },
+        $this.addMailsCsv = function (mailString, delimeter, elem) {
+            var mailArray = mailString.split(delimeter);
+            for (var i in mailArray) {
+                $this.addMail(mailArray[i],elem);
+            }
+        },
+                
         this.filter(".multiMail").each(function () {
-            var input = $(this);
+            input = $(this);
             input.css("position", "relative");
-            input.css("margin-top", "20px");
+            input.css("margin-top", "10px");
+            input.css("margin-left", "10px");
             input.parent(".mmOuter").css("margin-top", "20px");
-            input.attr("placeholder","Comma separated emails");
+            input.attr("placeholder", "Comma separated emails");
             input.keydown(function (event) {
                 if (event.keyCode === 188 || event.keyCode === 13) {
                     event.preventDefault();
                     var raw = input.val();
                     var mail = raw.split(",")[0];
-                    var condition = validateMail(mail, result)["condition"];
-                    var msg = validateMail(mail, result)["msg"]
-                    if (condition) {
-                        input.siblings(".mmBoxes").append("<span class='mmBox'>" + mail + " <img class='deleteBox' width=10 src='image/delete.png' style='cursor: pointer; margin-left: 10px;'/></span>");
-                        $.fn.multiMailInput.arrangeBoxCss(".mmBox");
-                        $(".deleteBox").click(function(){
-                            deleteMMBox(this);
-                        });
-                        
-                        result.push(mail);
-                        input.val("");
-                    } else {
-                        alert(msg);
-                    }
+                    $this.addMail(mail, input);
                 }
             });
         });
@@ -33,12 +51,21 @@
         return {
             getMails: function () {
                 return result;
+            },
+            addMail: function(mail){
+                $this.addMail(mail, input);
+            },
+            addMailArray: function(array){
+                $this.addMailsArray(array, input);
+            },
+            addMailCSV: function(mailString, delimeter){
+                $this.addMailsCsv(mailString, delimeter, input);
             }
-        }
+        };
     };
 
     var defaults = {
-    }
+    };
 
     function validateMail(mail, array) {
         // returns boolean after validating array of emails
@@ -48,11 +75,11 @@
         condition = true;
         if (!re.test(mail)) {
             condition = false;
-            msg = "Mail is not valid!";
+            msg = "\""+ mail + "\" is not valid!";
         }
-        if(array.indexOf(mail) !== -1){
+        if (array.indexOf(mail) !== -1) {
             condition = false;
-            msg += "Mail is already exist!";
+            msg += "\""+ mail + "\" is already exist!";
         }
         return {
             "condition": condition,
@@ -61,13 +88,13 @@
     }
 
     function deleteMMBox(elem) {
-        var itemToDel = $(elem).parent(".mmBox").text().replace(/ /g,'');
+        var itemToDel = $(elem).parent(".mmBox").text().replace(/ /g, '');
         while (result.indexOf(itemToDel) !== -1) {
-          result.splice(result.indexOf(itemToDel), 1);
+            result.splice(result.indexOf(itemToDel), 1);
         }
         $(elem).parent(".mmBox").remove();
     }
-    
+
     $.fn.multiMailInput.arrangeBoxCss = function (elem) {
         e = $(elem);
         e.css("margin", "10px");
@@ -76,6 +103,8 @@
         e.css("border-radius", "2px");
         e.css("background-color", "#F5F5F5");
         e.css("color", "black");
+        e.css("display", "block");
+        e.css("display", "block");
     };
 
 }(jQuery));
